44db3befb83c47011b3f2c7169a07a1e8638cc11
avtoriscenaria <fafterty@gmail.com>
2021-07-03 16:37:46 +0300

    add Info Component

---
+++ front/src/modules/Game/Info/components/InfoComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+
+const InfoComponent: React.FC = () => {
+  return <Paper> </Paper>;
+};
+
+export default InfoComponent;
---
+++ front/src/modules/Game/Info/containers/InfoContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import InfoComponent from "../components/InfoComponent";
+
+const DeskContainer: React.FC = () => {
+  return <InfoComponent />;
+};
+
+export default DeskContainer;
